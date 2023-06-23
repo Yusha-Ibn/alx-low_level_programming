f2cd6cc1729530564509aebe12c5475e9a2ad2bd
Yusha-Ibn <yushainc790@gmail.com>
2023-06-22 23:40:03 -0700

    Debugging

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -12,5 +12,5 @@ int main(void)
         i = 98;
         positive_or_negative(i);
 
-        return (0);
+return (0);
 }
